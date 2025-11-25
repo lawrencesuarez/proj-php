a5288fadbf99dc21110b4789d00e6b040137c978
lawrencesuarez <lwrncprstsrz@gmail.com>
2025-11-25 11:43:38 +0800

    Initial commit

---
+++ first.php
@@ -0,0 +1,15 @@
+<?php 
+$num1 = 20;
+$num2 = 37;
+
+$sum = $num1 + $num2;
+$diff = $num1 - $num2;
+$prod = $num1 * $num2;
+$quo = $num1 / $num2;
+
+echo "The sum of " .$num1. " and " .$num2. " is " .$sum. "<br>";
+echo "The difference of " .$num1. " and " .$num2. " is " .$diff. "<br>";
+echo "The product of " .$num1. " and " .$num2. " is " .$prod. "<br>";
+echo "The quotient of " .$num1. " and " .$num2. " is " .$quo. "<br>";
+
+?>
